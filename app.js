3971984842163f5c27a7f9ef8496a44a66071e0b
giorgikrgb <giorgikalabegashvili24@gmail.com>
2026-05-08 10:27:51 +0400

    meore

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 //1 პუნქტი
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
+let Number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i <=10; i++) {
+    console.log([i]);
+}
+
+//2 პუნქტი
+    let Number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,];
+    let i=0;
+    while (i <=15) {
+        console.log([i]);
+        i++;
+        
+    }
 }
