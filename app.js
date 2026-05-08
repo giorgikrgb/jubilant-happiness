716f49428f685a0067e0b9d4b2fd50072114c3cf
giorgikrgb <giorgikalabegashvili24@gmail.com>
2026-05-08 10:47:22 +0400

    hf

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ for (let i = 0; i < Number.length; i++) {
    let secondNumber = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,];
 
     while (i < secondNumber.length) {
-        console.log([i]);
+        console.log(secondNumber[i]);
         i++;
         
     }
