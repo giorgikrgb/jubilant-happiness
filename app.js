2128450211df242a19de8edf8a66550c049f0225
giorgikrgb <giorgikalabegashvili24@gmail.com>
2026-05-08 10:32:07 +0400

    mesame

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 //1 პუნქტი
-let Number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let Number = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for (let i = 0; i <=10; i++) {
     console.log([i]);
 }
 
 //2 პუნქტი
-    let Number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,];
+    let Number = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,];
     let i=0;
     while (i <=15) {
         console.log([i]);
         i++;
         
     }
-}
+//3 პუნქტი
+let Number = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let i = 0;
+do {
+    
+} while (i < Number.length) {
+    console.log([i]);
+    i++;
+}   
