946d3dca248a68574db97e08a0d80466ee53be1e
giorgikrgb <giorgikalabegashvili24@gmail.com>
2026-05-08 10:57:27 +0400

    metxe da mexute

--- app.js
+++ app.js
@@ -24,3 +24,31 @@ do {
 
 //4 პუნქტი
  const numbers = [45, 56, 78, 89, 101, 234, 333, 457];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i]);
+    }   
+
+    //5 პუნქტი
+    const currenday= new Date().getDay();
+    switch (currenday) {
+        case 0:            console.log("დღე კვირა");
+            break;  
+        case 1:            console.log("დღე ორშაბათი");
+            break;
+        case 2:            console.log("დღე სამშაბათი");
+            break;
+        case 3:            console.log("დღე ოთხშაბათი");
+            break;
+        case 4:            console.log("დღე ხუთშაბათი");
+            break;
+        case 5:            console.log("დღე პარასკევი");
+            break;
+        case 6:            console.log("დღე შაბათი");
+            break;
+
+        default:           console.log("არასწორი დღე");
+            break;
+    }
+}
